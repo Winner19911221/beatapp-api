327332782c1c47a36f1cb1dc126de46edcacc8a6
DELL <DELL@DESKTOP-EPBM9H3>
2026-04-30 13:37:04 -0400

    fix server limpio

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('API funcionando ??');
+  res.send('API funcionando 🚀');
 });
 
 app.listen(PORT, '0.0.0.0', () => {
